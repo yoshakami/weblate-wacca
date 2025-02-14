5db8f1e8892da7f9271072f0bbef17a4a32eb8ec
Yuuka <62856297+yoshakami@users.noreply.github.com>
2025-02-14 10:09:58 +0100

    reset po

--- locale/wacca_weblate_take_french.py
+++ locale/wacca_weblate_take_french.py
@@ -20,15 +20,37 @@ for file in os.listdir("en_US"):
         file_lines_fr[file] = po.readlines() # this contains \n
 
 for file in os.listdir("fr_BE"):
-    for i in range(len(file_lines_vanilla[file])):
+    lines_id = []
+    for i in range(2, len(file_lines_vanilla[file])):
         if "msgstr" in file_lines_vanilla[file][i]:
             print(i, file)
-            file_lines_vanilla[file][i] = "msgstr" + file_lines_vanilla[file][i - 1][5:] 
-            file_lines_en[file][i] = "msgstr" + file_lines_en[file][i - 1][5:]
-            file_lines_ja[file][i] = "msgstr" + file_lines_ja[file][i - 1][5:]
-            file_lines_fr[file][i-1] = file_lines_vanilla[file][i - 1]
-            file_lines_en[file][i-1] = file_lines_vanilla[file][i - 1]
-            file_lines_ja[file][i-1] = file_lines_vanilla[file][i - 1]
+            end = ''
+            end_id = 0
+            if file_lines_ja[file][i - 1][-1:] == '.' and file_lines_vanilla[file][i - 1][-1:] != '.':
+                end = '.'
+            if file_lines_vanilla[file][i - 1][-1:] == '.' and file_lines_ja[file][i - 1][-1:] != '.':
+                end_id = -1
+            if file_lines_vanilla[file][i - 1][:end_id] + end in lines_id: 
+                print(file_lines_vanilla[file][i - 1][:end_id])
+                file_lines_en[file][i] = ""
+                file_lines_ja[file][i] = ""
+                file_lines_fr[file][i] = ""
+                file_lines_vanilla[file][i] = ""
+                file_lines_fr[file][i-1] = ""
+                file_lines_en[file][i-1] = "" 
+                file_lines_ja[file][i-1] = ""
+                file_lines_vanilla[file][i-1] = ""
+                continue
+            print('writing line')
+            lines_id.append(file_lines_vanilla[file][i - 1][:end_id] + end)
+            file_lines_en[file][i] = "msgstr" + file_lines_en[file][i - 1][5:end_id] + end
+            file_lines_ja[file][i] = "msgstr" + file_lines_ja[file][i - 1][5:end_id] + end
+            file_lines_fr[file][i] = 'msgstr ""'
+            file_lines_vanilla[file][i] = "msgstr" + file_lines_vanilla[file][i - 1][5:end_id] + end
+            file_lines_fr[file][i-1] = "\n" + file_lines_vanilla[file][i - 1][:end_id] + end
+            file_lines_en[file][i-1] = "\n" + file_lines_vanilla[file][i - 1][:end_id] + end
+            file_lines_ja[file][i-1] = "\n" + file_lines_vanilla[file][i - 1][:end_id] + end
+            file_lines_vanilla[file][i-1] = "\n" + file_lines_vanilla[file][i - 1][:end_id] + end
     with open(os.path.join("fr_BE", file), "w", encoding="utf-8") as po:
         po.write(''.join(file_lines_vanilla[file]))
     with open(os.path.join("fr_FR", file), "w", encoding="utf-8") as po:
